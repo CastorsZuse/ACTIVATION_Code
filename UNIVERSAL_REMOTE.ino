867d76bc83c3ee856582885cda42fe2425f2e094
Nolan Cash <76930360+CastorsZuse@users.noreply.github.com>
2021-04-09 10:59:55 -0400

    Update UNIVERSAL_REMOTE.ino

--- UNIVERSAL_REMOTE.ino
+++ UNIVERSAL_REMOTE.ino
@@ -1,6 +1,10 @@
+//SWARM BOTS UNIVERSAL REMOTE FOR TESTING 
+//UPDATED TO 3.1 LIB
+//FOR MORE INFO CHECK OUT SwarmBots.online
+
+#include <Arduino.h>
 #include <ButtonDebounce.h>
 #include <IRremote.h>
-IRsend IrSender;
 
 #if defined(ARDUINO_ARCH_SAMD)
 #define Serial SerialUSB
@@ -20,6 +24,7 @@ const int ZAPPERgreenPin = 14;
 const int ZAPPERredBluePin = 15;
 const int ZAPPERredGreenPin = 18;
 const int ZAPPERblueGreenPin = 19;
+const int IR_SEND_PIN = 5;
 const int debounceDelay = 10;
 int buttonState = 0;   
 
@@ -42,10 +47,15 @@ void setup() {
     
      
     Serial.begin(115200);
-#if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB) || defined(SERIAL_PORT_USBVIRTUAL)
-    delay(2000);
+    Serial.begin(115200);
+#if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB) || defined(SERIAL_PORT_USBVIRTUAL)  || defined(ARDUINO_attiny3217)
+    delay(4000); // To be able to connect Serial monitor after reset or power up and before first printout
 #endif
-    Serial.println(F("START " __FILE__ " from " __DATE__));
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK); // Specify send pin and enable feedback LED at default feedback LED pin
+
     Serial.print(F("Ready to send IR signals at pin "));
     Serial.println(IR_SEND_PIN);
 }
@@ -64,72 +74,74 @@ uint32_t botZAPPERredGreen = 0xFF38C7;
 uint32_t botZAPPERblueGreen = 0xFF5AA5;
 
 uint8_t nbits = 32;
+uint8_t sCommand = 0x34;
 uint8_t sRepeats = 0;
 
   void loop() {
+   
     if (digitalRead(ACTIVATIONPin) == HIGH){
     delay(debounceDelay);
-    IrSender.sendNEC(botON, nbits);
+    IrSender.sendNEC(botON, sCommand, sRepeats);
     delay(600);
 }
       if (digitalRead(BUMPleftPin) == HIGH){
       delay(debounceDelay);
-      IrSender.sendNEC(botLEFT, nbits);
+      IrSender.sendNEC(botLEFT, sCommand, sRepeats);
       delay(600);
 }
         if (digitalRead(STALLPin) == HIGH){
         delay(debounceDelay);
-        IrSender.sendNEC(botSTALL, nbits);
+        IrSender.sendNEC(botSTALL, sCommand, sRepeats);
         delay(600);
 }
           if (digitalRead(BUMPrightPin) == HIGH){
           delay(debounceDelay);
-          IrSender.sendNEC(botRIGHT, nbits);
+          IrSender.sendNEC(botRIGHT, sCommand, sRepeats);
           delay(600);
 }
             if (digitalRead(NOPEleftPin) == HIGH){
             delay(debounceDelay);
-            IrSender.sendNEC(botNOPEleft, nbits);
+            IrSender.sendNEC(botNOPEleft, sCommand, sRepeats);
             delay(600);
 }
               if (digitalRead(NOPEbackPin) == HIGH){
               delay(debounceDelay);
-              IrSender.sendNEC(botNOPEback, nbits);
+              IrSender.sendNEC(botNOPEback, sCommand, sRepeats);
               delay(600);
 }
                 if (digitalRead(NOPErightPin) == HIGH){
                 delay(debounceDelay);
-                IrSender.sendNEC(botNOPEright, nbits);
+               IrSender.sendNEC(botNOPEright, sCommand, sRepeats);
                 delay(600);
 }
                   if (digitalRead(ZAPPERredPin) == HIGH){
                   delay(debounceDelay);
-                  IrSender.sendNEC(botZAPPERred, nbits);
+                  IrSender.sendNEC(botZAPPERred, sCommand, sRepeats);
                   delay(600);
 }
                     if (digitalRead(ZAPPERgreenPin) == HIGH){
                     delay(debounceDelay);
-                    IrSender.sendNEC(botZAPPERgreen, nbits);
+                    IrSender.sendNEC(botZAPPERgreen, sCommand, sRepeats);
                     delay(600);
 }
                       if (digitalRead(ZAPPERbluePin) == HIGH){
                       delay(debounceDelay);
-                      IrSender.sendNEC(botZAPPERblue, nbits);
+                      IrSender.sendNEC(botZAPPERblue, sCommand, sRepeats);
                       delay(600);
 }
                         if (digitalRead(ZAPPERredBluePin) == HIGH){
                         delay(debounceDelay);
-                        IrSender.sendNEC(botZAPPERredBlue, nbits);
+                       IrSender.sendNEC(botZAPPERredBlue, sCommand, sRepeats);
                         delay(600);
 }
                           if (digitalRead(ZAPPERredGreenPin) == HIGH){
                           delay(debounceDelay);
-                          IrSender.sendNEC(botZAPPERredGreen, nbits);
+                          IrSender.sendNEC(botZAPPERredGreen, sCommand, sRepeats);
                           delay(600);
 }
                             if (digitalRead(ZAPPERblueGreenPin) == HIGH){
                             delay(debounceDelay);
-                            IrSender.sendNEC(botZAPPERblueGreen, nbits);
+                            IrSender.sendNEC(botZAPPERblueGreen, sCommand, sRepeats);
                             delay(600);
   }
 }
